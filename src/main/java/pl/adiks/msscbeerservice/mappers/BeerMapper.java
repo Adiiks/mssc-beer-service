9b0356b220e03a8f922bf6b471d96423c5ebd861
Adiks <apacholak97@interia.pl>
2021-03-04 21:14:26 +0100

    implementing MapStruct

---
+++ src/main/java/pl/adiks/msscbeerservice/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package pl.adiks.msscbeerservice.mappers;
+
+import org.mapstruct.Mapper;
+import pl.adiks.msscbeerservice.entity.Beer;
+import pl.adiks.msscbeerservice.model.BeerDTO;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDTO beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDTO beerDTO);
+}
